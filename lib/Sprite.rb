eec8194ca5da8ea8d63023bc3dd052c0151026af
koyexes <koyagabriel@gmail.com>
2017-10-01 01:18:44 +0100

    render views and add helper functions

--- lib/Sprite.rb
+++ lib/Sprite.rb
@@ -3,21 +3,23 @@ require 'Sprite/controller'
 require 'Sprite/utility'
 
 module Sprite
-  def call(env)
-    @req = Rack::Request.new env
-    path = @req.path_info
-    request_method = @req.request_method.downcase
-    return [500, {}, []] if path == '/favicon.ico'
-    controller, action = get_controller_and_action_for(path, request_method)
-    response = controller.new.send(action)
-    [200, { 'Content-Type' => 'text/html' }, [response]]
-  end
+  class Application
+    def call(env)
+      @req = Rack::Request.new env
+      path = @req.path_info
+      request_method = @req.request_method.downcase
+      return [500, {}, []] if path == '/favicon.ico'
+      controller, action = get_controller_and_action_for(path, request_method)
+      response = controller.new.send(action)
+      [200, { 'Content-Type' => 'text/html' }, [response]]
+    end
 
-  def get_controller_and_action_for(path, verb)
-    _, controller, action, _others = path.split('/', 4)
-    require "#{controller.downcase}_controller.rb"
-    controller = Object.const_get(controller.capitalize! + 'Controller')
-    action = action.nil? ? 'index' : action.to_s
-    [controller, action]
+    def get_controller_and_action_for(path, verb)
+      _, controller, action, _others = path.split('/', 4)
+      require "#{controller.downcase}_controller.rb"
+      controller = Object.const_get(controller.capitalize! + 'Controller')
+      action = action.nil? ? 'index' : action.to_s
+      [controller, action]
+    end
   end
 end
